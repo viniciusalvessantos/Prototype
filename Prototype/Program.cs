7050443439b5df83f09243f763fb7e790be2e746
Vinicius Alves Santos <34243820+viniciusalvessantos@users.noreply.github.com>
2024-10-07 23:28:16 -0300

    ajustes

--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -9,13 +9,8 @@ class Program
         p1.BirthDate = Convert.ToDateTime("1977-01-01");
         p1.Name = "Jack Daniels";
         p1.idInfo = new IdInfo(666);
-
-        // Perform a shallow copy of p1 and assign it to p2.
         Person p2 = p1.ShallowCopy();
-        // Make a deep copy of p1 and assign it to p3.
         Person p3 = p1.DeepCopy();
-
-        // Display values of p1, p2 and p3.
         Console.WriteLine("Original values of p1, p2, p3:");
         Console.WriteLine("   p1 instance values: ");
         DisplayValues(p1);
@@ -23,9 +18,6 @@ class Program
         DisplayValues(p2);
         Console.WriteLine("   p3 instance values:");
         DisplayValues(p3);
-
-        // Change the value of p1 properties and display the values of p1,
-        // p2 and p3.
         p1.Age = 32;
         p1.BirthDate = Convert.ToDateTime("1900-01-01");
         p1.Name = "Frank";
